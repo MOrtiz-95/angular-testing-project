3228a0e8e9b233b2ce863fae814e2c6eb0e24307
Martin Ortiz <josephortiz.7cb@gmail.com>
2025-04-24 16:41:57 -0600

    Finalizado?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+interface Task {text: string}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,13 +10,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-testing-project';
 
-  addTask(task: string) {
+  tasks: Task[]= [];
+  selectedTask: string | null= null;
+  newTaskText: string | null= null;
 
+  addTask(task: string) {
+    const newTask: Task = {
+      text: this.newTaskText
+    }
+    this.tasks.push(newTask)
+    this.newTaskText = ""
   }
 
   deleteTask(task: string) {
+  this.selectedTask = null
+  this.newTaskText = ""
   }
 
+  editTask(task: Task): void {
+    this.selectedTask = { ...task }
+    this.newTaskText = task.text
+  }
+
+  updateTask(): void {
+  }
+ 
   getTasks(): string[] {
     return [];
   }
